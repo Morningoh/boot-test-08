5874a4b86d3be92dfa5420da72bfa2c413e2c573
liml <349603820@qq.com>
2024-08-31 10:53:19 +0800

    测试idea提交代码

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,13 +1,24 @@
 package com.example.controller;
 
+import com.example.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    User user01;
+
     @GetMapping("/hello")
     public String handle01() {
         return "hello";
+    }
+
+
+    @GetMapping("/user")
+    public User user01() {
+        return user01;
     }
 }
